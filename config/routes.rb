1b2b94cc043ef5118591604bd777a5ad0c2ed080
Wellington <wellingtonaperez@gmail.com>
2017-08-29 16:57:40 +0000

    Create mailchimp form and content to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'grayscales/index'
+  root to: 'grayscales#index'
 end
