6e8b64e4873946961e118038c629267ef058ccd6
Wellington <wellingtonaperez@gmail.com>
2017-08-29 20:34:30 +0000

    adding fonts

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
-
 module ScheduleLandscape
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
